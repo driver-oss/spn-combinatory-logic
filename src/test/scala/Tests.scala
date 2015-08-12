f282b966b72335e72556c9c5b6806ef78059666f
Stewart Stewart <stewinsalot@gmail.com>
2015-08-11 20:38:19 -0700

    fixed bug in test for step by step reduction

--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -102,7 +102,7 @@ object Tests extends TestSuite {
 
       "step by step reduction"-{
         /*assert(
-          evalHead(S * (K * S) * K * p * q * r) == (S * (K * S) * K * p * q * r),
+          evalHead(S * (K * S) * K * p * q * r) == (K * S * p * (K * p) * q * r),
           evalHead(K * S * p * (K * p) * q * r) == (S * (K * p) * q * r),
           evalHead(S * (K * p) * q * r)         == (K * p * r * (q * r)),
           evalHead(K * p * r * (q * r))         == (p * (q * r))
